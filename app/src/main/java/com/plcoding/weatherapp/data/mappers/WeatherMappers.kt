e474bb102a0a8551a9085c5e98d90e0d01100b45
flaviusgafincu <102972618+flaviusgafincu@users.noreply.github.com>
2023-05-25 11:19:31 +0300

    Update WeatherMappers.kt

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -8,12 +8,15 @@ import com.plcoding.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+// Define a private data class to hold indexed weather data
 private data class IndexedWeatherData(
     val index: Int,
     val data: WeatherData
 )
 
+// Extension function to map WeatherDataDto to a map of weather data
 fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
+    // Map each time entry to IndexedWeatherData object
     return time.mapIndexed { index, time ->
         val temperature = temperatures[index]
         val weatherCode = weatherCodes[index]
@@ -32,19 +35,26 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
             )
         )
     }.groupBy {
+        // Group the weather data by day
         it.index / 24
     }.mapValues {
+        // Extract only the weather data from IndexedWeatherData objects
         it.value.map { it.data }
     }
 }
 
+// Extension function to map WeatherDto to WeatherInfo
 fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    // Get the weather data map
     val weatherDataMap = weatherData.toWeatherDataMap()
+    
+    // Get the current weather data based on the current time
     val now = LocalDateTime.now()
     val currentWeatherData = weatherDataMap[0]?.find {
         val hour = if(now.minute < 30) now.hour else now.hour + 1
         it.time.hour == hour
     }
+    // Return the WeatherInfo object
     return WeatherInfo(
         weatherDataPerDay = weatherDataMap,
         currentWeatherData = currentWeatherData
