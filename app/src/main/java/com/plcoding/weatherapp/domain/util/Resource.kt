b52c15622ea38a2fe2ebe4abc422d64534bf5c29
flaviusgafincu <102972618+flaviusgafincu@users.noreply.github.com>
2023-05-25 11:55:23 +0300

    Update Resource.kt

--- app/src/main/java/com/plcoding/weatherapp/domain/util/Resource.kt
+++ app/src/main/java/com/plcoding/weatherapp/domain/util/Resource.kt
@@ -1,6 +1,10 @@
 package com.plcoding.weatherapp.domain.util
 
+// Sealed class representing a resource with data and optional message
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    // Subclass representing a successful resource with optional data
     class Success<T>(data: T?): Resource<T>(data)
+    
+    // Subclass representing an error resource with a message and optional data
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
 }
