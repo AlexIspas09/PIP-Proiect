cc9bf7f14120549b478c3965790ac6f51b6c9512
flaviusgafincu <102972618+flaviusgafincu@users.noreply.github.com>
2023-05-25 11:28:32 +0300

    Update WeatherRepositoryImpl.kt

--- app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -7,12 +7,15 @@ import com.plcoding.weatherapp.domain.util.Resource
 import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import javax.inject.Inject
 
+// Implementation of the WeatherRepository interface
 class WeatherRepositoryImpl @Inject constructor(
     private val api: WeatherApi
 ): WeatherRepository {
 
+    // Retrieve weather data for the given latitude and longitude
     override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
         return try {
+            // Try to get the weather data from the API and map it to WeatherInfo
             Resource.Success(
                 data = api.getWeatherData(
                     lat = lat,
@@ -20,6 +23,7 @@ class WeatherRepositoryImpl @Inject constructor(
                 ).toWeatherInfo()
             )
         } catch(e: Exception) {
+            // If an exception occurs, handle it and return an error resource
             e.printStackTrace()
             Resource.Error(e.message ?: "An unknown error occurred.")
         }
